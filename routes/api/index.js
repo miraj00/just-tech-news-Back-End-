9b056a46fb8574344850827d25b723687e6e60fa
miraj <miraj00@gmail.com>
2021-09-09 16:05:10 -0400

    updated routes to include comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,13 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
+
+// add new endpoint here
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-
+router.use('/comments', commentRoutes);
 
 
 
